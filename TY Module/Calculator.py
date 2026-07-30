17f0482d69053d9e3b69c9421babc31e9c191b77
MsMeade <134280029+MsMeade@users.noreply.github.com>
2026-07-30 10:15:11 +0100

    Add files via upload

---
+++ TY Module/Calculator.py
@@ -0,0 +1,8 @@
+num1=float(input("Please enter your first number: "))
+num2=float(input("Please enter your second number: "))
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(round((num1/num2),2))
+
---
+++ TY Module/Variables.py
@@ -0,0 +1,8 @@
+name= "Barry"
+age = 20
+place = "Australia"
+school="Sydney High School"
+
+print("My name is",name)
+print("I am",age,"years old. \nI am from Meath.")
+print("I teach in",school)
